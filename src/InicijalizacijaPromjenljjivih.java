9fa96de2eba060da208f7735549ce16d9f730da6
Milorad Kecman <Milorad Kecman>
2017-02-16 22:55:21 +0100

    Prva modifikacija

---
+++ src/InicijalizacijaPromjenljjivih.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Milorad Kecman
+ */
+public class InicijalizacijaPromjenljjivih {
+    public static void main (String args[]){
+        int vrijednost = 7;
+        int duplaVrijednost = vrijednost * 2;
+        float kolicnikSaDva = vrijednost / 2;
+        char znakPodatka;
+        znakPodatka = 'M';
+        
+        System.out.println("vrijednost= " + vrijednost + "\n"
+                            + "duplaVrijednost= " + duplaVrijednost + "\n"
+                            + "kolicnikSaDva= " + kolicnikSaDva + "\n"
+                            + "znakPodatka= " +znakPodatka
+                                    
+                            );
+         
+                            
+                            
+    }
+    
+}
